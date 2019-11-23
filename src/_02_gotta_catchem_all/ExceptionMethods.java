4b30b71c236077db1af21a0ef481b764a5b44bde
League Student <league@iMac-13.attlocal.net>
2019-11-23 11:27:54 -0800

    finished module 3

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,23 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+
+	void divide(double a, double b) throws IllegalArgumentException {
+		
+		
+		if (b == 0.0) {
+			throw new IllegalArgumentException();
+		}
+
+	}
+
+	String reverseString(String s) throws IllegalStateException {
+
+		if (s.isEmpty()) {
+			throw new IllegalStateException();
+		}
+
+		return s;
+	}
+
 }
